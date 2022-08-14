a1b68479221fba78f05745bd9fbb9c7f8175088c
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-14 11:29:45 +0600

    1st Program Commit

---
+++ BasicConcepts/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BasicConcepts
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Input();
+        }
+
+        public static void Input()
+        {
+            Console.Write("Enter Bengali marks: ");
+            double bengali = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter English marks: ");
+            double english = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Mathematics marks: ");
+            double math = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Computer Studies marks: ");
+            double computer = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Science marks: ");
+            double science = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Average Result: {CalculateResult(bengali, english, math, computer, science)}");
+        }
+        
+        public static double CalculateResult(double bengali, double english, double math, double computer, double science)
+        {
+            return  (bengali + english + math + computer + science) / 5.0;
+        }
+    }
+}
