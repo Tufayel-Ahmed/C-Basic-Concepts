36b8740fae6bb1f9eedfdb173e960b6bbedc4e9f
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-14 13:19:34 +0600

    Switch Case Commit

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Input();
+        }
+
+        public static void Input()
+        {
+            Console.Write("Enter your letter: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            checkLetter(ch);
+        }
+
+        public static void checkLetter(char ch)
+        {
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'A':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'E':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'I':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'O':
+                    Console.WriteLine("Vowel");
+                    break;
+                case 'U':
+                    Console.WriteLine("Vowel");
+                    break;
+                default:
+                    Console.WriteLine("Consonant");
+                    break;
+            }
+        }
+    }
+}
